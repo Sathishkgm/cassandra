7474449189f3226e17de19cac285ac18c18903df
rajofchennai <rajofchennai@yahoo.com>
2012-02-04 23:38:41 +0530

    changed version of simple uuid

--- lib/cassandra.rb
+++ lib/cassandra.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 gem 'thrift_client', :git => 'git@github.com:rajofchennai/thrift_client.git'
 require 'thrift_client'
-gem 'simple_uuid' , '~> 0.2.0'
+gem 'simple_uuid' , '~> 0.1.0'
 require 'simple_uuid'
 
 require 'json' unless defined?(JSON)
